eccc8f0a3307ce5062d381be4c1c3bd45d83ded5
Diogo Silas <diogosilas4@gmail.com>
2024-01-23 17:12:08 -0300

    first commit

---
+++ assets/scripts/javascript/objects/screen.js
@@ -0,0 +1,33 @@
+const screen = {
+    userProfile: document.querySelector('.profile-data'),
+    renderUser(user) {
+        this.userProfile.innerHTML =
+        `<div class="info">
+            <img src="${user.avatarUrl}" alt="Foto de Perfil do Usuário" />
+            <div class="data">
+                <h1>${user.name ?? 'Não possui nome.'}</h1>
+                <p>${user.bio ?? 'Não possui bio.'}</p>
+                <p><a href="${user.blog ?? '#'}" target="_blank">Blog</a></p>
+            </div>
+        </div>`
+
+        let repositoriesItems = '';
+        user.repositories.forEach(repo => {
+            repositoriesItems += 
+            `<li><a href="${repo.html_url}" target="_blank">${repo.name}</a></li>`
+        });
+
+        if(user.repositories.length > 0) {
+            this.userProfile.innerHTML += 
+            `<div class="repositories section">
+                <h2>Repositótrios</h2>
+                <ul>${repositoriesItems}</ul>
+            
+            </div>`
+        };
+    }
+};
+
+export { 
+    screen
+}
